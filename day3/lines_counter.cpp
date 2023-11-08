f5666672f6820432dcf27620af844f5fb9563e5c
Anthony Rabine <anthony@rabine.fr>
2023-11-08 16:50:55 +0100

    updated to be more generic with IFileVisitor

--- day3/lines_counter.cpp
+++ day3/lines_counter.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <fstream>
 #include <filesystem>
 #include <vector>
 
@@ -14,21 +15,31 @@ public:
 
     }
 
+    bool isDirectory() const {
+        return m_isDirectory;
+    }
+
+    void setDirectory(bool isDirectory)
+    {
+        m_isDirectory = isDirectory;
+    }
+
     std::string getFileName() const {
         return m_name;
     }
     
 private:
     std::string m_name;
+    bool m_isDirectory{false};
 };
 
 
-class IDisplayVisitor
+class IFileVisitor
 {
 public:
-    virtual ~IDisplayVisitor() {}
+    virtual ~IFileVisitor() {}
 
-    virtual void print(const File &dir) = 0;
+    virtual void visit(const File &dir) = 0;
 };
 
 
@@ -43,7 +54,7 @@ public:
 
     }
 
-    void scan(IDisplayVisitor &v)
+    void scan(IFileVisitor &v)
     {
         int directoryCount = 0;
         int fileCount = 0;
@@ -53,12 +64,14 @@ public:
              
             for (auto const& dir_entry : fs::directory_iterator{ m_dir_path, fs::directory_options::skip_permission_denied })
             {
-            
+                File f(fs::absolute(dir_entry.path()));
+
                 if (dir_entry.is_directory()) 
                 {
                     directoryCount++;
                     Directory subdir(dir_entry.path());
                     subdir.scan(v);
+                    f.setDirectory(true);
                 }
                 
                 if (dir_entry.is_regular_file())
@@ -66,11 +79,8 @@ public:
                     fileCount++;
                     // Open file and count lines if expected extension
                 }
-                
-                File f(fs::path(dir_entry.path()).filename());
-                v.print(f);
+                v.visit(f);
                 m_files.push_back(f);
-
             }
         }
         catch (...)
@@ -85,27 +95,68 @@ private:
     
 };
 
-
-class ConsoleDirectoryDisplay : public IDisplayVisitor
+// Implémentation de type display, on se contente d'afficher le nom du fichier
+class ConsoleFileDisplay : public IFileVisitor
 {
-   virtual void print(const File &f)
+public:
+   virtual void visit(const File &f)
    {
          std::cout << "File: " << f.getFileName() << std::endl;
    }
 };
 
+class LinesCounter : public IFileVisitor
+{
+public:
+   virtual void visit(const File &f)
+   {
+        std::string extension = fs::path(f.getFileName()).extension();
+        if (!f.isDirectory() && (extension == ".txt"))
+        {
+            std::cout << "Opening: " << f.getFileName() << std::endl;
+            std::ifstream file(f.getFileName());
+            if (!file.is_open()) {
+                std::cerr << "Erreur : Impossible d'ouvrir le fichier " << f.getFileName() << std::endl;
+                return;
+            }
+
+            int lineCount = 0;
+            std::string line;
+
+            while (std::getline(file, line)) {
+                lineCount++;
+            }
+
+            file.close();
+
+            m_totalLines += lineCount;
+        }
+   }
+
+    int getTotalLines() const {
+        return m_totalLines;
+    }
+
+private:
+    int m_totalLines{0};
+};
+
 int main(int argc, char **argv)
 {
-
-    
     if (argc > 1)
     {
         std::cout << "Scaning directory: " << fs::absolute(fs::path(argv[1])) << std::endl;
         Directory dir(argv[1]);
 
-        ConsoleDirectoryDisplay display;
-
+        ConsoleFileDisplay display;
         dir.scan(display);
+
+        std::cout << "------------------------------" << std::endl;
+
+        LinesCounter counter;
+        dir.scan(counter);
+
+        std::cout << "Total lines: " << counter.getTotalLines() << std::endl;
     }
 
     return 0;
